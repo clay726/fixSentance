6d2d9cec7f7a955a7853c750003f928f6b95e4bd
clay726 <clay726@gmail.com>
2017-05-09 10:46:46 -0700

    fjah

---
+++ fixSentance/functions-assignments.py
@@ -0,0 +1,34 @@
+import sys
+
+def fixSentance(aString):
+	strings = aString.split('.')
+	for temp in strings:
+		if temp:
+			temp = temp.replace('_',' ').strip().lower().capitalize()
+			print temp.split(' ')
+			#temp = temp.split(' ')
+		try:
+			if ((temp[-1] != '.') & (temp[-1] != '!') & (temp[-1] != '?')):
+				temp = temp + '.'
+			print temp
+		except IndexError:
+			print "Caught index error"
+
+stringToFix = str(sys.argv[1])
+
+def splitString(str):
+	str = str.split(' ')
+	str = ' '.join(str)
+	str = str.split('.')
+	str = ' '.join(str)
+	str = str.split('?')
+	str = ' '.join(str)
+	str = str.split(',')
+	str = ' '.join(str)
+	str = str.split('!')
+	str = ' '.join(str)
+	words = str.split(' ')
+	print words 
+	return words
+
+print fixSentance(stringToFix) 
