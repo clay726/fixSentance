76793eccead3ac464598499240f3124863952cf1
clay726 <clay726@gmail.com>
2017-05-11 10:42:15 -0700

    commit msg

--- functions-assignments.py
+++ functions-assignments.py
@@ -15,9 +15,19 @@ def fixSentance(aString):
 			print "Caught index error"
 
 stringToFix = str(sys.argv[1])
+split = splitString(stringToFix)
 
-dict = open("dictionary.txt")
-dict.close()
+def spellCheck(dictionary, words):
+#given a list of words in a sentance and a dictionary of correcty spelled words
+#output a list of words that are in the dictionary
+	return list(set(dictionary) & set(words)
+	correctWords = []
+	for word in words:
+		for thing in dictionary:
+			if (word == thing):
+				print words
+				correctWords += word
+
 
 def splitString(str):
 	str = str.split(' ')
@@ -33,5 +43,9 @@ def splitString(str):
 	words = str.split(' ')
 	print words 
 	return words
+
+dict = open("dictionary.txt")
+wl = dict.readlines()
+dict.close()
 
 print fixSentance(stringToFix) 
