ce8088753d5b4a1ce38195c285e088deb39847f5
clay726 <clay726@gmail.com>
2017-06-06 11:13:38 -0700

    Done I Guess

--- functions-assignments.py
+++ functions-assignments.py
@@ -7,50 +7,39 @@ def fixSentance(aString):
 		if temp:
 			temp = temp.replace('_',' ').strip().lower().capitalize()
 			temp = re.sub(r" +", " ", temp)
-			print temp.split(' ')
-			#Make it so that if only lowers non names 
-			temp = temp.split(' ')
 		try:
 			if ((temp[-1] != '.') & (temp[-1] != '!') & (temp[-1] != '?')):
-				temp = (temp[-1] + '.')
-			print temp
+				temp = (temp + '.')
+			return temp
 		except IndexError:
 			print "Caught index error"
 
 def spellCheck(dictionary, words):
-#given a list of words in a sentance and a dictionary of correcty spelled words
-#output a list of words that are in the dictionary
-	#return list(set(dictionary) & set(words)
 	correctWords = []
-	print "Got to spell check"
+	#print "Got to spell check"
 	for word in words:
-		#print word
+		word = word.lower()
 		for thing in dictionary:
-			#print word
-			#print thing
-			#print type(word)
-			#print type(thing)
 			if ((word == thing) & (word != " ") & (word != "")):
-				print word
-				#print thing
-				correctWords += word
-	print "Leaving spellcheck"
+				correctWords.append(word)
+	#print "Leaving spellcheck"
+	return correctWords
+	
 
 def nameCheck(names, words):
-	print "Enter Namecheck"
+	#print "Enter Namecheck"
 	correctNames = []
-	for name in words:
-		print name
-		for thing in names:
-			#print name
+	for name in names:
+		for word in words:
+			#print word
 			#put a capitalize function in there
-			if ((name == thing) & (name != "  ") & (name == "")):
-				print name
-				correctNames += name
-	print "Exit Namecheck"
+			if ((name == word) & (word != "  ") & (word != "")):
+				correctNames.append(name)
+	#print "Exit Namecheck"
+	return correctNames
 
 def fixAbbreviation(aString):
-	temp = aString.replace('Dr.','Doctor').replace('','')	
+	temp = aString.replace('Dr.','Doctor').replace('Rev.','Reverend').replace('Prof.','Professor').replace('St.','Saint').replace('Gen.','General').replace('Adm.','Admiral').replace('Cpt.','Captain').replace('Gen.','General')
 
 
 def splitString(str):
@@ -65,7 +54,6 @@ def splitString(str):
 	str = str.split('!')
 	str = ' '.join(str)
 	words = str.split(' ')
-	print words 
 	return words
 
 stringToFix = str(sys.argv[1])
@@ -76,7 +64,6 @@ wl = dict.readlines()
 dict.close()
 
 names = open("names.txt")
-#butt = names.lower()
 nl = names.readlines()
 names.close()
 
@@ -88,7 +75,14 @@ nameList = []
 for name in nl:
 	nameList.append(name.strip())
 
-spellCheck(wordList, split)
-nameCheck(nameList, split)
+x = nameCheck(nameList, split)
+x = ' '.join(x)
+y = spellCheck(wordList, split)
+y = ' '.join(y)
 
-print fixSentance(stringToFix)
+a = fixSentance(y)
+
+b = fixSentance(x)
+
+print a
+print b
